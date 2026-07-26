9f609c35af5108cb5e2b45b7c7436e5c59aeb4ca
Rahul Giri <rahulgiri6022@gmail.com>
2026-07-26 18:21:45 +0530

    Time: 2 ms (16.64%) | Memory: 42.8 MB (32.68%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -9,44 +9,35 @@ class MyQueue {
     }
     
     public void push(int x) {
-        
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
         s1.push(x);
-        size++;
+        while(!s2.isEmpty()){
+            s1.push(s2.pop());
+
+        }
+        
+       
+        
+       
         
        
         
     }
     
     public int pop() {
-        if(s2.isEmpty()){
-            if(!s1.isEmpty()){
-                for(int i=1;i<=size;i++){
-            s2.push(s1.pop());
-
-            }
-             
-        }
-
-           
-        }
-        size--;
        
-        return s2.pop();
+       
+        return s1.pop();
         
     }
     
     public int peek() {
 
-        if(s2.isEmpty()){
-            if(!s1.isEmpty()){
-                for(int i=1;i<=size;i++){
-            s2.push(s1.pop());
-        }
-            }
-           
-        }
+       
          
-        return s2.peek();
+        return s1.peek();
         
     }
     
